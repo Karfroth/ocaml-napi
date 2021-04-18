98b81a4bc6a4ed1142ab1362ed1489474f569f20
Woosang Lee <recollect12@gmail.com>
2021-04-17 22:01:12 -0400

    rename V1 module

--- lib/Experimental.ml
+++ lib/Experimental.ml
@@ -1,5 +1,5 @@
 open Ctypes
 open Foreign
-open Util
+open V1
 
 let node_api_get_module_file_name = foreign "node_api_get_module_file_name" (napi_env @-> ptr_opt(string) @-> returning napi_status)
--- lib/V3.ml
+++ lib/V3.ml
@@ -1,16 +1,16 @@
 open Ctypes
 open Foreign
-include Util
+include V1
 
 (* Exceptions *)
 module ErrorHandling = struct
-  include Util.ErrorHandling
+  include V1.ErrorHandling
 
   let napi_fatal_exception = foreign "napi_fatal_exception" (napi_env @-> napi_value @-> returning napi_status)
 end
 
 module ObjectLifetime = struct
-  include Util.ObjectLifetime
+  include V1.ObjectLifetime
 
   (* Cleanup on exit of the current Node.js instance *)
   let napi_add_env_cleanup_hook = foreign "napi_add_env_cleanup_hook" (napi_env @-> (funptr (ptr(void) @-> returning void) @-> ptr(void) @-> returning napi_status))
@@ -19,7 +19,7 @@ end
 
 module Asynchronous = struct
   open Types
-  include Util.Asynchronous
+  include V1.Asynchronous
 
   let napi_open_callback_scope = foreign "napi_open_callback_scope" (napi_env @-> napi_value @-> napi_async_context @-> ptr(napi_callback_scope) @-> returning napi_status)
   let napi_close_callback_scope = foreign "napi_close_callback_scope" (napi_env @-> napi_callback_scope @-> returning napi_status)
